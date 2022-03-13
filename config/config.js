ae79b908688b2215d767a3d3b02911ff6024c448
Your Name <tungvip31@gmail.com>
2022-03-13 11:08:47 +0700

    bai 1

--- config/config.js
+++ config/config.js
@@ -1,1 +1,43 @@
+var fs = require('fs');
+module.exports = {
+    port: 3000,
+    createServer: function (req, res) {
+        // console.log(req.data);
+        if (req.url == '/') {
+            res.writeHead(200, {
+                "Context-type": "text/html"
+            })
+            fs.createReadStream('./views/login.html').pipe(res);
+        } else if (req.url == '/dasboard.html') {
+            // console.log(req);
+            req.on('data', function (chunk) {
+                let data = '' + chunk
+                const check1 = JSON.parse('{"' + decodeURI(data).replace(/"/g, '\\"').replace(/&/g, '","').replace(/=/g, '":"') + '"}')
+                var user = fs.readFileSync('user.json')
+                const check = JSON.parse(user.toString())
+                if (check[0].userName != check1.userName) {
+                    res.writeHead(200, {
+                        "Context-type": "text/html"
+                    })
+                    fs.createReadStream('./views/login wrongusername.html').pipe(res)
+                } else if (check[0].password != check1.password) {
+                    res.writeHead(200, {
+                        "Context-type": "text/html"
+                    })
+                    fs.createReadStream('./views/login wrongpassword.html').pipe(res)
+                } else {
+                    res.writeHead(200, {
+                        "Context-type": "text/html"
+                    })
+                    fs.createReadStream('./views/dasboard.html').pipe(res)
+                }
+            });
+        } else {
+            res.writeHead(404, {
+                "Context-type": "text/html"
+            })
+            fs.createReadStream('./views/404.html').pipe(res);
+        }
 
+    }
+}
